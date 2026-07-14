cbb0c14db56e7700483634558a8fb0e6354bf01b
Anil Kondaveni <Anil.kondaveni@techcarrot.ae>
2026-07-14 17:37:40 +0530

    added

--- techcarrotOdoo_1/ticketing_it/models/account_file.py
+++ techcarrotOdoo_1/ticketing_it/models/account_file.py
@@ -7,12 +7,14 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def action_print_invoices_as_zip(self):
-        report = self.env.ref('account.account_invoices')
+        report = self.env.ref('techcarrot_invoice.action_generate_techcarrot_invoice_report')
         buffer = io.BytesIO()
 
         with zipfile.ZipFile(buffer, 'w', zipfile.ZIP_DEFLATED) as zf:
             for move in self:
-                pdf_content, _ = report._render_qweb_pdf(report.report_name, [move.id])
+                pdf_content, _ = self.env['ir.actions.report']._render_qweb_pdf(
+                    report.id, [move.id]
+                )
                 filename = f"{move.name.replace('/', '_')}.pdf"
                 zf.writestr(filename, pdf_content)
 
